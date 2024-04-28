1bb390da84f57afb486b5f0afd820dc7658e0e6d
Sagar Tambrahalli <sagar.uu97@gmail.com>
2024-04-28 09:23:56 +0200

    Completed Auth

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,27 +1,44 @@
-import React from "react";
 import Link from "next/link";
+import { auth } from "@clerk/nextjs/server";
+import { UserButton } from "@clerk/nextjs";
 
-const Header = () => {
+const Header = async () => {
+  const { userId } = auth(); //Clerk auth - Check if user is logged in or not
+
   return (
-    <div>
-      <nav className="flex items-center justify-between px-6 py-4 mb-5 bg-green-800">
-        <div className="flex items-center">
-          <Link href="/">
-            <div className="text-lg font-bold text-white uppercase">
-              Gaia.eco
-            </div>
+    <nav className="flex items-center justify-between px-6 py-4 mb-5 bg-blue-700">
+      <div className="flex items-center">
+        <Link href="/">
+          <div className="text-lg font-bold text-white uppercase">Gaia.eco</div>
+        </Link>
+      </div>
+      <div className="flex items-center text-white">
+        {!userId && (
+          <>
+            <Link
+              href="sign-in"
+              className="text-gray-300 hover:text-white mr-4"
+            >
+              Sign In
+            </Link>
+            <Link
+              href="sign-up"
+              className="text-gray-300 hover:text-white mr-4"
+            >
+              Sign Up
+            </Link>
+          </>
+        )}
+        {userId && (
+          <Link href="profile" className="text-gray-300 hover:text-white mr-4">
+            Profile
           </Link>
+        )}
+        <div className="ml-auto">
+          <UserButton afterSignOutUrl="/" />
         </div>
-        <div className="text-white">
-          <Link href="sign-in" className="text-gray-300 hover:text-white mr-4">
-            Sign In
-          </Link>
-          <Link href="sign-up" className="text-gray-300 hover:text-white mr-4">
-            Sign Up
-          </Link>
-        </div>
-      </nav>
-    </div>
+      </div>
+    </nav>
   );
 };
 
