cb8808faaadf0d2956ab48b306f79612f22b6aae
Sagar Tambrahalli <sagar.uu97@gmail.com>
2024-04-28 14:44:27 +0200

    Updated analytics page

--- app/dashboard/analytics2/page.tsx
+++ app/dashboard/analytics2/page.tsx
@@ -5,6 +5,15 @@ const Analytics = () => {
   return (
     <div>
       <Navbar />
+      <div className="py-4 px-3">
+        <div className="border border-gray-400 py-4 px-2">
+          <p className="text-3xl"> Project 2 Title</p>
+          <p className="text-sm">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos
+            aspernatur officia praesentium molestias ex accusamus commodi.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
